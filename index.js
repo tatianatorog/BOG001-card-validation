151f53dfe01790454e261b462cfae5605b2a4091
TATIANA Toro <tatianatorog@gmail.com>
2020-06-23 10:51:12 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,75 @@
+import validator from './validator.js';
+
+
+for (let i= 1; i<=12; i++){
+    let monthOption = document.createElement('option') 
+    monthOption.value= i;
+    monthOption.innerText= i;
+    let month = document.getElementById("month");
+    month.appendChild(monthOption)
+
+}
+
+const yearActual =new Date().getFullYear();
+for(let i= yearActual; i<= yearActual +10; i++){
+    let yearOption = document.createElement('option') 
+    yearOption.value= i;
+    yearOption.innerText= i;
+    let year = document.getElementById("year");
+    year.appendChild(yearOption); 
+}
+
+
+
+
+let result = document.getElementById("validar");
+result.addEventListener("click", checkForm);
+
+function checkForm () {
+    let userCard = document.getElementById("creditCard");//*
+    let userName = document.getElementById("userName");//*
+    let userCcv = document.getElementById("input-code");//*
+    let expressionNumber = /^([0-9])*$/;//*
+    if (userCard.value === "" || userName.value === "" || userCcv.value === "") {
+        alert(" Todos los campos son obligatorios");
+        return false;
+    } else if (userCard.value.length < 13 ) {
+        alert("Revisa número tarjeta de crédito");
+        return false;
+    } else if (!expressionNumber.test(userCard.value)) {
+        alert("Debes digitar números en el campo Tarjeta de credito");
+        return false;
+    } else if (!expressionNumber.test(userCcv.value) || userCcv.value.length < 3 || userCcv.value.length > 3) {
+        alert("Revisa tu código de seguridad");
+        return false;
+        // }else if (!expressionNumber.test(month.value) || month.value <1 || month.value > 12) {
+        // alert ("Ingresa un mes valido ");
+        // return false
+        // }else if (!expressionNumber.test(year.value) || year.value < 20 || year.value >30) {
+        //   alert ("Ingresa un año valido ");
+        //   return false
+    } else {
+        //LLamado a la función que enmascara el numero de tarjeta
+        let maskNumber = validator.maskify(userCard.value);
+
+        let container = document.getElementById("contenedor");
+        container.style.display = "none";
+        let answer = document.getElementById("answerContainer");
+        answer.style.display = "block";
+
+        //LLamado a la funcion que valida mediante el algoritmo de Luhn el numero de tarjeta
+        let validNumber = validator.isValid(userCard.value);
+        let esValid = "";
+        if (validNumber === true) {
+            
+            esValid = `VALIDA<br><img src=images/check1.png><br>!Gracias por confiar en nosotros¡`;
+        } else {
+            esValid = `INVALIDA<br><img src= images/wrong.png><br>¡Intentalo de nuevo!`;
+        }
+        //Imprime respuesta del servicio de validación
+        answer.innerHTML = "<h2>" + userName.value.toUpperCase() + " TU TARJETA DE CRÉDITO " + " " + "" + maskNumber + " " + "<br>" + "ES" + " " + esValid;
+    }
+
+}
+
+console.log(validator);
---
+++ validator.js
@@ -0,0 +1,65 @@
+//  Función para validar el numero de tarjeta mediante el algoritmo de luhn
+const validator = {
+
+    isValid: function luhn(cardNumber) {
+
+    let individualNumbers = [];
+    let duplicateNumbers = [];
+    let finalArry = undefined;
+    let sum = 0;
+    // let validCard = false;
+
+    //  A traves del método split dividimos el numero de la  tarjeta(que es un string) en una matriz de cadenas
+    //  mediante la separación de la cadena en sub cadenas
+
+    let cardNumberSplit = cardNumber.split("").reverse();
+    // console.log(cardNumber);
+    // console.log(cardNumberSplit);
+
+
+    // if (cardNumber.length >=13 ) {
+        // Creamos un for para poder iterar con cada una de las sub cadenas del array
+        for ( let i = 0; i < cardNumberSplit.length; i++) {
+            if (i % 2 == 0) {
+                // console.log(i);
+                individualNumbers.push(cardNumberSplit[i]);// Usamos método push() para los elementos que cumplen con la condición  al final de un nuevo arreglo
+                
+            } else {
+                duplicateNumbers.push((cardNumberSplit[i] * 2));
+                console.log("numeros posicion2 multiplicados" + duplicateNumbers)
+            }
+        }
+
+    // }
+    // El método join nos permite pasar un string a un array  y luego aplicamos el método split de nuevo 
+    //De esta manera cada numero es un solo dígito y se convierte en array otra vez
+
+    duplicateNumbers = duplicateNumbers.join("").split("");
+    console.log("join" + duplicateNumbers.join(""))
+    // console.log("numeros convertidos en array " + duplicateNumbers)
+    finalArry = duplicateNumbers.concat(individualNumbers);
+    console.log("array para sumar" + finalArry)
+    for (let j = 0; j <= finalArry.length-1; j++) {
+        sum += parseInt(finalArry[j]);
+        console.log(sum);
+    }
+
+    if (sum % 10 === 0) {
+        // validCard = true;
+        return true;  
+    } else {
+        return false;
+      }
+
+    
+ 
+},
+
+//Enmascaramiento del numero de la tarjeta de crédito
+  maskify: function mask(cardNumber) {
+    let newNumber = cardNumber.replace(/\d(?=\d{4})/g, "#");
+    return newNumber;
+}
+};
+
+export default validator;
